c9c97d67fc5ced3ea055ea850e331aff17f85139
snehalreddy <syelmat@gmail.com>
2020-10-02 23:13:57 +0530

    Data injection into the dialog done.

--- src/app/training/current-training/stop-training.component.ts
+++ src/app/training/current-training/stop-training.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-stop-training',
   template: ` <h1 mat-dialog-title>Are you sure?</h1>
+    <mat-dialog-content>
+      You have already completed {{ passedData.progress }}% of the workout.
+    </mat-dialog-content>
     <mat-dialog-actions>
       <button mat-raised-button [mat-dialog-close]="true">Yes</button>
       <button mat-raised-button [mat-dialog-close]="false">No</button>
     </mat-dialog-actions>`,
 })
-export class StopTrainingComponent {}
+export class StopTrainingComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) {}
+}
